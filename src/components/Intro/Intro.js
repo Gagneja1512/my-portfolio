988680d4376c29bc1cca4a205910bd38ff1abcf2
gagneja1512 <aadityaarora1215@gmail.com>
2022-06-09 17:35:24 +0530

    Animation to navbar

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -14,7 +14,7 @@ const Intro = () => {
                     <span>Currently pursuing Computer science and engineering from National Institute of Technology, Hamirpur.I'm an executive member at App Team of our institute.Lives in Hanumangarh , Rajasthan</span>
                 </div>
 
-                <button className="button i-button">CV</button>
+                <button className="button i-button">Download CV</button>
                 <div className="i-icons">
                     <a href="https://github.com/Gagneja1512"><img src={Github} alt="" /></a>
                     <img src={LinkedIn} alt="" />
