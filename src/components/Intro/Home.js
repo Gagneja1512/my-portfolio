5cf3d64213ce36b9489825f6c2df17a2e5cc91f5
gagneja1512 <aadityaarora1215@gmail.com>
2022-12-28 23:36:24 +0530

    Making Home UI

--- src/components/Intro/Home.js
+++ src/components/Intro/Home.js
@@ -4,8 +4,8 @@ import {Link} from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div className="home">
-            <section>
+        <div>
+            <section className="home">
                 <div className="home__container container grid">
                     <div className="home__social">
                         <span className="home__social-follow">Follow Me</span>
@@ -25,8 +25,37 @@ const Home = () => {
                     <div className="home__date">
                         <h1 className="home__title">Hi , I'am Aaditya Gagneja</h1>
                         <h3 className="home__subtitle">Web Developer</h3>
-                        <p className="home__description">Lorem Epsoum Lorem EpsoumLorem EpsoumLorem EpsoumLorem EpsoumLorem EpsoumLorem EpsoumLorem EpsoumLorem Epsoum</p>
-                        <Link to='/about'>More About me</Link>
+                        <p className="home__description">A passionate web developer. Currently pursuing Computer Science and Enginnering in National Institute of Technology , Hamirpur.</p>
+                        <Link className="buttons" to='/about'><i className="uil uil-user button__icon"></i>More About me</Link>
+                    </div>
+
+                    <div className="my__info">
+                        <div className="info__item">
+                            <i className="uil uil-facebook-messenger info__icon"></i>
+
+                            <div>
+                                <h3 className="info__title">Messenger</h3>
+                                <span className="info__subtitle">user.user</span>
+                            </div>
+                        </div>
+
+                        <div className="info__item">
+                            <i className="uil uil-whatsapp info__icon"></i>
+
+                            <div>
+                                <h3 className="info__title">Whatsapp</h3>
+                                <span className="info__subtitle">9876543210</span>
+                            </div>
+                        </div>
+
+                        <div className="info__item">
+                            <i className="uil uil-envelope-edit info__icon"></i>
+
+                            <div>
+                                <h3 className="info__title">Email</h3>
+                                <span className="info__subtitle">email@gmail.com</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
